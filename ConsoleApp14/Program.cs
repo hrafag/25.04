ec1af711dd19c20cf050d41c7685cc8c0c17a1e7
hrafag <afagnd@code.edu.az>
2022-04-25 15:49:01 +0400

    The new project is added

---
+++ ConsoleApp14/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Student student = new Student("Faik", "Aliyev");
+            Console.WriteLine(student.GetStudents());
+        }
+
+        class Student
+        {
+            public string Name;
+            public string Surname;
+
+            public Student(string name, string surname)
+            {
+                Name = name;
+                Surname = surname;
+            }
+
+            public string GetStudents()
+            {
+            return $"Name:{Name}, Surname: {Surname}";
+            }
+        }
+    }
+}
